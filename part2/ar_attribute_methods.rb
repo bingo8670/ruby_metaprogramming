f044f7986093f5bbc0e10c0aef265b08c1e0060c
bingo8670 <bingo891111@gmail.com>
2018-05-11 12:37:34 +0800

    chapter 12

---
+++ part2/ar_attribute_methods.rb
@@ -0,0 +1,18 @@
+require "active_record"
+ActiveRecord::Base.establish_connection :adapter  => "sqlite3",
+                                        :database => "dbfile"
+
+ActiveRecord::Base.connection.create_table :tasks do |t|
+  t.string  :description
+  t.boolean :completed
+end
+
+class Task < ActiveRecord::Base; end
+
+task = Task.new
+task.description = "Clean up garage"
+task.completed   = true
+task.save
+
+p task.description       # => "Clean up garage"
+p task.completed?        # => true
